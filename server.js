5017b9a6975a9b3f5ffc9ede11dee7d0a4732708
hankzimmer7 <hankzimmer7@gmail.com>
2018-07-25 21:53:21 -0400

    adds initial friend data

---
+++ server.js
@@ -0,0 +1,36 @@
+// ==============================================================================
+// DEPENDENCIES
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// ==============================================================================
+
+// Creates express server
+var app = express();
+
+// Sets initial port
+var PORT = process.env.PORT || 1337;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// Points the server to the routing files
+// ================================================================================
+
+// require("./routing/apiRoutes")(app);
+// require("./routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("Friend Finder app listening at http://localhost:" + PORT);
+});
