98a4269939b3b68bdf41f6ba58f4c6b8e1bdcbf0
hankzimmer7 <hankzimmer7@gmail.com>
2018-07-26 20:25:07 -0400

    added input validation

--- app/data/friends.js
+++ app/data/friends.js
@@ -28,7 +28,7 @@ var friendArray = [
         "4",
         "1",
         "5",
-        "3",
+        "1",
         "5",
         "4",
         "3",
@@ -44,11 +44,107 @@ var friendArray = [
         "1",
         "3",
         "5",
+        "3",
+        "2",
+        "1",
+        "2",
+        "2",
+        "1"
+      ]
+    },
+    {
+      "name": "Barret",
+      "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/a/ac/Barret_PortraitNB.png/revision/latest?cb=20130203032221",
+      "scores":[
+        "1",
+        "1",
+        "3",
+        "5",
+        "4",
+        "4",
+        "1",
+        "2",
+        "3",
+        "1"
+      ]
+    },
+    {
+      "name": "Cait Sith",
+      "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/6/65/CaitSith-FFVIIArt.png/revision/latest?cb=20110227161509",
+      "scores":[
+        "3",
+        "2",
+        "3",
+        "5",
+        "1",
+        "4",
+        "1",
+        "2",
+        "3",
+        "5"
+      ]
+    },
+    {
+      "name": "Yuffie",
+      "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/7/7a/Yuffie_Portrait.jpg/revision/latest?cb=20130203034936",
+      "scores":[
+        "1",
+        "1",
+        "4",
+        "5",
+        "4",
+        "2",
+        "2",
+        "2",
+        "3",
+        "5"
+      ]
+    },
+    {
+      "name": "Vincent",
+      "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/8/81/Vincent_Portrait.jpg/revision/latest?cb=20130203034546",
+      "scores":[
+        "3",
+        "1",
         "4",
         "2",
         "1",
         "2",
-        "3",
+        "2",
+        "1",
+        "5",
+        "5"
+      ]
+    },
+    {
+      "name": "Red XIII",
+      "photo": "https://vignette.wikia.nocookie.net/finalfantasy2/images/8/8e/RedXIII_Portrait_FFVII.jpg/revision/latest?cb=20090424170007&path-prefix=de",
+      "scores":[
+        "2",
+        "1",
+        "4",
+        "2",
+        "1",
+        "1",
+        "2",
+        "4",
+        "5",
+        "2"
+      ]
+    },
+    {
+      "name": "Sephiroth",
+      "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/4/4c/Sephiroth_Portrait.jpg/revision/latest?cb=20130203034009",
+      "scores":[
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
         "1"
       ]
     }
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -82,10 +82,11 @@ module.exports = function (app) {
 
   app.post("/api/reset", function () {
     // Reset the friend data back to the original set
-    friendData = [{
+    friendData = [
+      {
         "name": "Cloud",
         "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/3/32/Cloud_Portrait.jpg/revision/latest?cb=20080926214004",
-        "scores": [
+        "scores":[
           "2",
           "2",
           "4",
@@ -101,12 +102,12 @@ module.exports = function (app) {
       {
         "name": "Tifa",
         "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/0/0a/Tifa_Portrait.jpg/revision/latest?cb=20130203034455",
-        "scores": [
+        "scores":[
           "2",
           "4",
           "1",
           "5",
-          "3",
+          "1",
           "5",
           "4",
           "3",
@@ -117,17 +118,113 @@ module.exports = function (app) {
       {
         "name": "Aeris",
         "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/e/e4/Aeris_Portrait.jpg/revision/latest?cb=20080926214002",
-        "scores": [
+        "scores":[
+          "3",
+          "1",
+          "3",
           "5",
+          "3",
+          "2",
+          "1",
+          "2",
+          "2",
+          "1"
+        ]
+      },
+      {
+        "name": "Barret",
+        "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/a/ac/Barret_PortraitNB.png/revision/latest?cb=20130203032221",
+        "scores":[
+          "1",
+          "1",
+          "3",
           "5",
+          "4",
           "4",
           "1",
           "2",
           "3",
-          "4",
+          "1"
+        ]
+      },
+      {
+        "name": "Cait Sith",
+        "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/6/65/CaitSith-FFVIIArt.png/revision/latest?cb=20110227161509",
+        "scores":[
           "3",
           "2",
-          "4"
+          "3",
+          "5",
+          "1",
+          "4",
+          "1",
+          "2",
+          "3",
+          "5"
+        ]
+      },
+      {
+        "name": "Yuffie",
+        "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/7/7a/Yuffie_Portrait.jpg/revision/latest?cb=20130203034936",
+        "scores":[
+          "1",
+          "1",
+          "4",
+          "5",
+          "4",
+          "2",
+          "2",
+          "2",
+          "3",
+          "5"
+        ]
+      },
+      {
+        "name": "Vincent",
+        "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/8/81/Vincent_Portrait.jpg/revision/latest?cb=20130203034546",
+        "scores":[
+          "3",
+          "1",
+          "4",
+          "2",
+          "1",
+          "2",
+          "2",
+          "1",
+          "5",
+          "5"
+        ]
+      },
+      {
+        "name": "Red XIII",
+        "photo": "https://vignette.wikia.nocookie.net/finalfantasy2/images/8/8e/RedXIII_Portrait_FFVII.jpg/revision/latest?cb=20090424170007&path-prefix=de",
+        "scores":[
+          "2",
+          "1",
+          "4",
+          "2",
+          "1",
+          "1",
+          "2",
+          "4",
+          "5",
+          "2"
+        ]
+      },
+      {
+        "name": "Sephiroth",
+        "photo": "https://vignette.wikia.nocookie.net/finalfantasy/images/4/4c/Sephiroth_Portrait.jpg/revision/latest?cb=20130203034009",
+        "scores":[
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1"
         ]
       }
     ];
